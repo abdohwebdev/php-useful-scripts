11a163da0305adf61e7ae513aaa61a2e8e8dca1e
abdurrahman <super12226@gmail.com>
2025-03-09 11:26:15 +0200

    add GET request function

--- api_networking/README.md
+++ api_networking/README.md
@@ -4,4 +4,4 @@ This directory contains PHP scripts for working with APIs and networking tasks. 
 
 ## Available Scripts
 
-- coming soon
+- **[get-request.php](https://github.com/abdohwebdev/php-useful-scripts/tree/main/api_networking/get-request.php)** - a function that sends a GET request and returns the response.
---
+++ api_networking/get-request.php
@@ -0,0 +1,23 @@
+<?php
+// function definition
+function sendGetRequest($url) {
+    $ch = curl_init($url);
+
+    // Set the cURL option to return the response as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+
+    if ($response === false) {
+        return 'cURL Error: ' . curl_error($ch);
+    } else {
+        return $response;
+    }
+
+}
+
+// Example usage
+$url = 'https://jsonplaceholder.typicode.com/posts/1';
+$response = sendGetRequest($url);
+echo 'Response: ' . $response;
+?>
